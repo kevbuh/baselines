f621f524dca224b8326d659763b061208c61a5b2
Kevin Buhler <buhlerkw@gmail.com>
2025-11-15 12:01:04 +0000

    cartpole a2c pytorch

--- reinforce.py
+++ reinforce.py
@@ -12,14 +12,10 @@ import torch.optim as optim
 from torch.distributions import Categorical
 
 parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
-parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
-                    help='discount factor (default: 0.99)')
-parser.add_argument('--seed', type=int, default=543, metavar='N',
-                    help='random seed (default: 543)')
-parser.add_argument('--render', action='store_true',
-                    help='render the environment')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='interval between training status logs (default: 10)')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G', help='discount factor (default: 0.99)')
+parser.add_argument('--seed', type=int, default=543, metavar='N', help='random seed (default: 543)')
+parser.add_argument('--render', action='store_true', help='render the environment')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N', help='interval between training status logs (default: 10)')
 args = parser.parse_args()
 
 render_mode = "human" if args.render else None
--- vae.py
+++ vae.py
@@ -8,16 +8,11 @@ from torchvision import datasets, transforms
 from torchvision.utils import save_image
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--no-accel', action='store_true', 
-                    help='disables accelerator')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N', help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: 10)')
+parser.add_argument('--no-accel', action='store_true', help='disables accelerator')
+parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N', help='how many batches to wait before logging training status')
 args = parser.parse_args()
 
 use_accel = not args.no_accel and torch.accelerator.is_available()
